d44847fb59710ec52ab980150f596fdc7ba8d865
WongZhiqiang <wongzq.dev@gmail.com>
2021-07-08 09:50:54 +0800

    A # 维护路径；

---
+++ Unity/Assets/Example/DataManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using etp.xr.Tools;
+using LitJson;
+using xr.StepByStep.Manager;
+
+namespace Assets.Example
+{
+    public class DataManager : SingletonMonoBehaviourClass<DataManager>, IDataManager
+    {
+        public void Awake()
+        {
+         
+        }
+
+        public void Start()
+        {
+          
+        }
+
+        public void Initialize(JsonData stepJsonData)
+        {
+           
+        }
+
+        public void StepMoveNext()
+        {
+          
+        }
+
+        public bool CheckCurrentStepComplete()
+        {
+            return true;
+        }
+    }
+}
